87836e2623e636fed1398e3ed4c9f3ffc23efdcc
nuqor <github@nuqor.de>
2024-10-22 15:56:00 +0200

    Add keymaps for Telescope and LSP

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -71,5 +71,33 @@
       options.silent = true;
     }
 
+    # Telescope
+    {
+      mode = "n";
+      key = "<C-p>";
+      action.__raw = "require('telescope.builtin').find_files";
+    }
+    {
+      mode = "n";
+      key = "<leader>fg";
+      action.__raw = "require('telescope.builtin').live_grep";
+    }
+    {
+      mode = "n";
+      key = "<leader>fb";
+      action.__raw = "require('telescope.builtin').buffers";
+    }
+    {
+      mode = "n";
+      key = "<leader>fh";
+      action.__raw = "require('telescope.builtin').help_tags";
+    }
+
+    # LSP
+    {
+      mode = "n";
+      key = "K";
+      action = "vim.lsp.buf.hover()";
+    }
   ];
 }
