050302ba01abc0322773ba2f8f6e8e6ab0b22f11
nuqor <github@nuqor.de>
2025-01-24 16:06:25 +0100

    Add editorconfig proerty: format_on_save

--- config/default.nix
+++ config/default.nix
@@ -38,4 +38,18 @@
     settings.flavour = "macchiato";
   };
 
+  editorconfig = {
+    enable = true;
+    properties = {
+      format_on_save = ''
+        function(bufnr, val)
+          assert(
+            val == 'true' or val == 'false',
+            'format_on_save must be either "true" or "false"'
+          )
+          vim.b[bufnr].format_on_save = val=='true' and true or false
+        end
+      '';
+    };
+  };
 }
