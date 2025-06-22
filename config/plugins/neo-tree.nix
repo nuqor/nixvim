5d4a55ef79d3a1f9ff91ee62a501f835a2f85b46
nuqor <github@nuqor.de>
2025-06-22 20:04:02 +0200

    disable lualine for neo-tree

--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -1,23 +1,30 @@
 { config, lib, ... }:
+
 let
   cfg = config.nuqor.nvim.neo-tree;
 in
+
 {
+
   options = {
     nuqor.nvim.neo-tree.enable = lib.mkEnableOption "Enable Neovim plugin: neo-tree";
   };
 
   config = lib.mkIf cfg.enable {
+
     nuqor.nvim.plenary.enable = lib.mkDefault true;
     nuqor.nvim.web-devicons.enable = lib.mkDefault true;
-    plugins = {
-      neo-tree = {
-        enable = true;
-        filesystem.filteredItems = {
-          alwaysShow = [ ".gitignore" ];
-          neverShow = [ ".DS_Store" ];
-        };
+
+    plugins.neo-tree = {
+      enable = true;
+      filesystem.filteredItems = {
+        alwaysShow = [ ".gitignore" ];
+        neverShow = [ ".DS_Store" ];
       };
     };
+
+    plugins.lualine.settings.options.disabled_filetypes = [ "neo-tree" ];
+
   };
+
 }
