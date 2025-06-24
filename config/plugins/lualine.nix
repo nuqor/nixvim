1741164f8e8a652b610ad3acc0fc897e13528936
nuqor <github@nuqor.de>
2025-06-24 15:37:32 +0200

    update lualine settings

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -17,6 +17,7 @@ in
     plugins = {
       lualine = {
         enable = true;
+        settings.options.theme = "catppuccin";
         settings.sections.lualine_x = [
           {
             __unkeyed-1.__raw = "require('noice').api.statusline.mode.get";
