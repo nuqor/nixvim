32177eb5a91547ae51dfae816b7c10c4d661e818
nuqor <github@nuqor.de>
2025-06-24 15:37:46 +0200

    update tin-inline-diagnostics settings

--- config/plugins/tiny-inline-diagnostic.nix
+++ config/plugins/tiny-inline-diagnostic.nix
@@ -1,15 +1,22 @@
 { config, lib, ... }:
+
 let
   cfg = config.nuqor.nvim.tiny-inline-diagnostic;
 in
+
 {
+
   options = {
     nuqor.nvim.tiny-inline-diagnostic.enable = lib.mkEnableOption "Enable Neovim plugin: tiny-inline-diagnostic";
   };
 
   config = lib.mkIf cfg.enable {
-    plugins = {
-      tiny-inline-diagnostic.enable = true;
+    plugins.tiny-inline-diagnostic = {
+      enable = true;
+      settings = {
+        preset = "classic";
+      };
     };
   };
+
 }
